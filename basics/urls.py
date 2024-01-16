984e49c08fd7cb0f7e48e2e8f185219361938e88
Franky0911 <franklinjamestqr@gmail.com>
2024-01-16 22:04:55 +0530

    djangolarge

--- basics/urls.py
+++ basics/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from basics import views
 urlpatterns = [
     path('sum/',views.sum),
+    path('calculator/',views.Calculate),
+    path('LS/',views.large),
 ]
--- basics/views.py
+++ basics/views.py
@@ -11,4 +11,48 @@ def sum(request):
     else:
         return render(request,"basics/demo.html")
     
-  
+
+def Calculate(request):
+    if request.method == "POST":
+        n1 = int(request.POST.get("txtn1"))
+        n2 = int(request.POST.get("txtn2"))
+        button = request.POST.get("btn")
+        if button == "+":
+            sum = n1 + n2
+            return render(request,"basics/calculator.html",{"result": sum})
+        elif button == "-":
+            diff = n1 - n2
+            return render(request,"basics/calculator.html",{"result": diff})
+        elif button == "*":
+            prod = n1 * n2
+            return render(request,"basics/calculator.html",{"result": prod})
+        elif button == "/":
+            div = n1 / n2
+            return render(request,"basics/calculator.html",{"result": div})
+        else:
+            return render(request,"basics/calculator.html")
+    else:
+        return render(request,"basics/calculator.html")
+
+
+def large(request):
+    if request.method == "POST":
+        n1 = int(request.POST.get("txtn1"))
+        n2 = int(request.POST.get("txtn2"))
+        n3 = int(request.POST.get("txtn3"))
+        if n1 > n2 and n1 > n3:
+            l = n1
+        elif n2 > n1 and n2 > n3:
+            l = n2
+        else:
+            l = n3
+
+        if n1 < n2 and n1 < n3 :
+            s = n1
+        elif n2 < n1 and n2 < n3:
+            s = n2
+        else:
+            s = n3
+        return render(request,"basics/ls.html",{"large": l,"small":s})                    
+    else:
+        return render(request,"basics/ls.html")
